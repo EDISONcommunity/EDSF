855c27031ac08f5a2af34cb8607d4eef3068c939
Yuri Demchenko <ydemchen@users.noreply.github.com>
2021-09-28 00:25:42 +0200

    Update Home.js

--- edsf-website/src/Components/Home.js
+++ edsf-website/src/Components/Home.js
@@ -900,16 +900,18 @@ export default function Home(props) {
             fontWeight="fontWeightLight"
             m={1}
             className={classes.headerText2}
-          >Maintaining the EDISON project legacy</Box>
-        </Box>
-        <Box
+          >Maintaining the EDISON project legacy
+            </Box>
+            <Box
             fontFamily="fontFamily"
             fontSize="h6.fontSize"
             fontWeight="fontWeightLight"
             m={1}
             className={classes.headerText2}
-          >Call for contribution EDSF Release 4 - <a href="https://github.com/EDISONcommunity/EDSF/wiki/EDSFhome">Link</a></Box>
+          >Call for contribution EDSF Release 4 - <a href="https://github.com/EDISONcommunity/EDSF/wiki/EDSFhome">Link</a>
         </Box>
+        </Box>
+        
         <Box className={classes.headerImgBox}>
           <img src={edsfImg} className={classes.headerImg} alt="title" />
         </Box>
